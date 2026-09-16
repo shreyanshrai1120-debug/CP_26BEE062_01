56037ca6956a48ca60165522d6a1736b98069ae4
shreyanshrai1120-debug <shreyanshrai1120@gmail.com>
2026-09-16 10:54:46 +0530

    Add files via upload

---
+++ printlargerandsmallernumber.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+int a,b;
+
+    printf("Enter two Numbers :");
+    scanf("%d %d",&a,&b);
+
+    if(a>b) printf("%d is greater and %d is smaller",a,b);
+    else if(b>a) printf("%d is greater than %d",b,a);
+      else if (a = b) printf(" the given number are equal");
+
+      
+
+return 0;
+    
+} 
